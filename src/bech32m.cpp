008da6a0a29d5610eea5c74a0c2b93e12e146eb8
Kristian Mika <kristian.mika@protonmail.com>
2022-03-11 23:31:26 +0100

    transform instead of loop

--- src/bech32m.cpp
+++ src/bech32m.cpp
@@ -1,5 +1,6 @@
 #include "bech32m.h"
 #include "bech32m_exception.h"
+#include <algorithm>
 #include <vector>
 
 static const std::string BECH_SYMBOLS = "qpzry9x8gf2tvdw0s3jn54khce6mua7l";
@@ -14,8 +15,7 @@ inline char encodeBechChar(const BechCharType chr) { return BECH_SYMBOLS[chr.to_
 std::string encodeHumanReadablePart(const BitStorage &storage) {
     std::vector<char> out;
 
-    for (const BechCharType val : storage) {
-        out.push_back(encodeBechChar(val));
-    }
+    std::transform(storage.begin(), storage.end(), std::back_inserter(out), encodeBechChar);
+
     return {out.begin(), out.end()};
 }
