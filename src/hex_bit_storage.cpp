3b99004ee8434bff003e313a51cf924f701e0c89
Kristian Mika <kristian.mika@protonmail.com>
2022-03-12 01:28:30 +0100

    feat: add Base64BitStorage

--- src/hex_bit_storage.cpp
+++ src/hex_bit_storage.cpp
@@ -1,11 +1,14 @@
 #include "hex_bit_storage.h"
 #include "bech32m_exception.h"
 
-uint8_t hex_2_int(char ch) {
+BechCharType hex_2_int(char ch) {
     if (ch >= '0' && ch <= '9') {
-        return ch - '0';
+        return BechCharType (ch - '0');
     }
-    return 10 + ch - 'a';
+    if (ch >= 'a' && ch <= 'f') {
+        return BechCharType (10 + ch - 'a');
+    }
+    throw Bech32mException("Invalid hex symbol: " + std::string(1, ch));
 }
 
 bool check_hex_string(const std::string &str) { return str.length() % 2 == 0 && str.find_first_not_of("0123456789ABCDEFabcdef"); }
@@ -16,19 +19,14 @@ HexBitStorage::HexBitStorage(const std::string &hex_value) {
     }
 
     for (int i = 0; i < hex_value.size(); ++i) {
-        uint8_t curr = hex_2_int(hex_value[i]);
+        BechCharType  curr = hex_2_int(hex_value[i]);
 
         for (int j = 0; j < 4; ++j) {
 
-            value.set(i * 4 + j, curr & (1 << j));
+            value.set(i * 4 + j, (curr & BechCharType (1 << j)).any());
         }
     }
     length = hex_value.size() * 4;
 
-    // add padding
-    int padding_length = 5 - (length % 5);
-    for (int i = 0; i < padding_length; ++i) {
-        value[length + i] = 0;
-    }
-    length += padding_length;
+    pad();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "bech32m.h"
 #include "hex_bit_storage.h"
+#include "base64_bit_storage.h"
 #include <iostream>
 int main() {
+    // HEX DEMO
     auto to_encode = "fafafa";
     HexBitStorage storage = HexBitStorage(to_encode);
 
@@ -10,7 +12,17 @@ int main() {
     }
     std::cout << std::endl;
 
-    // not finished!
     std::cout << "bech32m(" << to_encode << ") = " << encodeHumanReadablePart(storage) << std::endl;
+
+    // BASE64 demo
+    auto some_base64 = "aGkK";
+    Base64BitStorage base_storage = Base64BitStorage(some_base64);
+
+    for (const BechCharType val : base_storage) {
+        std::cout << val << ", "; // val.to_ulong() to get the numeric value
+    }
+    std::cout << std::endl;
+    std::cout << "bech32m(" << some_base64 << ") = " << encodeHumanReadablePart(base_storage) << std::endl;
+
     return 0;
 }
